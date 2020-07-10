daa399ed82dcb7135ce7a43ebb4e49122e26be2f
tokup672 <016023378398456luna@gmail.com>
2020-07-10 16:15:23 +0900

    登録画面まで動くように

--- src/main/java/jp/kobespiral/todo/controller/TodoController.java
+++ src/main/java/jp/kobespiral/todo/controller/TodoController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import jp.kobespiral.todo.entitiy.Todo;
 import jp.kobespiral.todo.form.CreateForm;
+import jp.kobespiral.todo.form.TodoForm;
 import jp.kobespiral.todo.service.TodoService;
 
 @Controller
@@ -16,12 +18,26 @@ public class TodoController {
   @Autowired
   TodoService ts;
 
-  @PostMapping("/todos")
+  @PostMapping("/todos/register")
   public String registerTodo(@ModelAttribute("CreateForm") @Validated CreateForm createform, Model model) {
       Todo todo = ts.createTodo(createform);
       model.addAttribute("todo", todo); 
       return "register_success";
   }
+
+  @GetMapping("/todolist")
+  public String showTodolist(Model model){
+      return "todolist";
+  }
+
+  @GetMapping("/register")
+  public String showRegister(Model model){
+    CreateForm createform = new CreateForm();
+    model.addAttribute("CreateForm",createform);
+    return "register";
+  }
+
+  // @GetMapping("/todos/register")
 
   
 }
--- src/main/java/jp/kobespiral/todo/repository/TodoRepository.java
+++ src/main/java/jp/kobespiral/todo/repository/TodoRepository.java
@@ -7,6 +7,6 @@ import jp.kobespiral.todo.entitiy.Todo;
 
 @Repository
 public interface TodoRepository extends CrudRepository<Todo, Long> {
-  public Iterable<Todo> findUserByUidLike(String uid);
-  public Iterable<Todo> findUserByNameLike(String name);
+  //public Iterable<Todo> findUserByUidLike(String uid);
+  //public Iterable<Todo> findUserByNameLike(String name);
 }
