ac35100c178b59ab16457988de76c3fcde1f8e6b
sekimoto_r <whxidzis@gmail.com>
2020-07-15 20:09:08 +0900

    update追加，ただしユーザ登録されていない人のlist表示はまだ無理なまま

--- src/main/java/jp/kobespiral/todo/controller/TodoController.java
+++ src/main/java/jp/kobespiral/todo/controller/TodoController.java
@@ -8,11 +8,13 @@ import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import jp.kobespiral.todo.entitiy.Todo;
 import jp.kobespiral.todo.form.CreateForm;
 import jp.kobespiral.todo.form.TodoForm;
+import jp.kobespiral.todo.form.UpdateForm;
 import jp.kobespiral.todo.service.TodoService;
 
 @Controller
@@ -26,15 +28,15 @@ public class TodoController {
     model.addAttribute("todo", todo);
     return "register_success";
   }
-  
+
   @GetMapping("/todolist")
   public String showTodolist(Model model) {
     List<TodoForm> todolist = ts.getTodoList();
-    System.out.println(todolist+"ssssssssssssssssssssssssssssssssssssss");
+
     model.addAttribute("tlist", todolist);
     return "todolist";
   }
-  
+
   @GetMapping("/register")
   public String showRegister(Model model) {
     CreateForm createform = new CreateForm();
@@ -42,6 +44,22 @@ public class TodoController {
     return "register";
   }
 
-  // @GetMapping("/todos/register")
+  // updateのhtmlに遷移するためのapi
+  @GetMapping("/todos/{tid}/update")
+  public String showUpdate(@ModelAttribute @PathVariable Long tid, Model model) {
+    UpdateForm updateForm = new UpdateForm();
+    updateForm.setUid(ts.getUidByTid(tid));
+    System.out.println(updateForm + "sssssssssssssssssssssssssssssssssssssssssssssss");
+    model.addAttribute("UpdateForm", updateForm);
+    return "update";
+  }
+
+  //updateのhtmlからもらったデータをもとに更新するためのAPI
+  @PostMapping("/todos/{tid}/update")
+  public String registerTodo(@ModelAttribute("UpdateForm") @Validated UpdateForm updateForm, Model model) {
+    Todo todo = ts.updateTodo(updateForm);
+    model.addAttribute("todo", todo);
+    return "register_success";
+  }
 
 }
--- src/main/java/jp/kobespiral/todo/service/TodoService.java
+++ src/main/java/jp/kobespiral/todo/service/TodoService.java
@@ -10,8 +10,10 @@ import org.springframework.stereotype.Service;
 import jp.kobespiral.todo.dto.TodoDto;
 import jp.kobespiral.todo.entitiy.Todo;
 import jp.kobespiral.todo.entitiy.User;
+import jp.kobespiral.todo.exception.UidException;
 import jp.kobespiral.todo.form.CreateForm;
 import jp.kobespiral.todo.form.TodoForm;
+import jp.kobespiral.todo.form.UpdateForm;
 import jp.kobespiral.todo.repository.TodoRepository;
 import jp.kobespiral.todo.repository.UserRepository;
 
@@ -22,44 +24,79 @@ public class TodoService {
   @Autowired
   UserRepository ur;
 
-  public Todo createTodo(CreateForm form){
+  public Todo createTodo(CreateForm form) {
     Date now = new Date();
     Todo todo = tr.save(new Todo(null, form.getTitle(), form.getDescription(), true, now, now, null, form.getUid()));
     return todo;
   }
-    
-    // List<UserDto> uids = this.getUserByuid(uid);
-    
-    //   if (uid != null && uids.isEmpty()){
-      //     User user = userRepository.save(new User(null, form.getUid(), form.getName(), now));
-      //     return UserDto.build(user);
-      // } else { 
-        //     throw new UidException("ユーザIDが重複しています");
-        // }
-      // }
-      
-      public List<TodoForm> getTodoList() {
-        ArrayList<TodoForm> list = new ArrayList<TodoForm>();
-        Date now = new Date();
-        // TodoForm todoform = new TodoForm((long)1,"name","title","des",true,now,now,now);
-        // list.add(todoform);
-      
-        for(Todo t:tr.findAll()){
-          TodoForm todoform = new TodoForm();
-          todoform.setTid(t.getId());
-          todoform.setCreatedAt(t.getCreatedAt());
-          todoform.setDescription(t.getDescription());
-          todoform.setDoneAt(t.getDonedAt());
-          for(User user : ur.findUserByUidLike(t.getUid())){
-            todoform.setName(user.getName());
-          }
-      
-          todoform.setOpen(t.isOpen());
-          todoform.setTitle(t.getTitle());
-          todoform.setUpdatedAt(t.getUpdatedAt());
-          list.add(todoform);
-        }
-        return list;
-      }
-}
 
+  // List<UserDto> uids = this.getUserByuid(uid);
+
+  // if (uid != null && uids.isEmpty()){
+  // User user = userRepository.save(new User(null, form.getUid(), form.getName(),
+  // now));
+  // return UserDto.build(user);
+  // } else {
+  // throw new UidException("ユーザIDが重複しています");
+  // }
+  // }
+
+  public List<TodoForm> getTodoList() {
+    ArrayList<TodoForm> list = new ArrayList<TodoForm>();
+    Date now = new Date();
+    // TodoForm todoform = new
+    // TodoForm((long)1,"name","title","des",true,now,now,now);
+    // list.add(todoform);
+
+    for (Todo t : tr.findAll()) {
+      TodoForm todoform = new TodoForm();
+      todoform.setTid(t.getId());
+      todoform.setCreatedAt(t.getCreatedAt());
+      todoform.setDescription(t.getDescription());
+      todoform.setDoneAt(t.getDonedAt());
+      for (User user : ur.findUserByUidLike(t.getUid())) {
+        todoform.setName(user.getName());
+      }
+
+      todoform.setOpen(t.isOpen());
+      todoform.setTitle(t.getTitle());
+      todoform.setUpdatedAt(t.getUpdatedAt());
+      list.add(todoform);
+    }
+    return list;
+  }
+
+  //uidでtidをとってくるだけのメソッド
+  public String getUidByTid(Long tid) {
+    Todo todo = tr.findById(tid).orElseThrow(() -> new UidException("No such tid"));
+    return todo.getUid();
+  }
+
+  //updateformの内容とDB内のtodoの内容を比較して，異なる点があれば差し替えるメソッド
+  public Todo updateTodo(UpdateForm updateForm){
+    Todo todo = tr.findById(updateForm.getTid()).orElseThrow(() -> new UidException("No such tid"));
+    Date now = new Date();
+    if(todo.getTitle()!=updateForm.getTitle()){
+      todo.setTitle(updateForm.getTitle());
+    }
+
+    if(todo.getDescription()!=updateForm.getDescription()){
+      todo.setDescription(updateForm.getDescription());
+    }
+
+    if(todo.isOpen()!=updateForm.isOpen()){
+      if(updateForm.isOpen()==true){
+        todo.setOpen(true);
+        todo.setDonedAt(null);
+      }else{
+
+        todo.setOpen(false);
+        todo.setDonedAt(now);
+      }
+    }
+    todo.setUpdatedAt(now);
+    tr.save(todo);
+    return todo;
+  }
+
+}
